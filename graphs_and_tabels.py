19a058b75f6b733293ca3690d82912e8a5e5e7ff
Dean Grosbard <dean.grosbard@Rashmikas-MBP.localdomain>
2019-08-03 12:05:14 -0700

    get gini table map

--- graphs_and_tabels.py
+++ graphs_and_tabels.py
@@ -2152,7 +2152,7 @@ if __name__ == '__main__':
 
     base_cols= ['policy','rho','timestamp','m','n','exp_no','size','structure']
 
-    sbpss_cd_table1()
+    sbpss_cd_table1('erdos_renyi_sbpss_comp_alis')
     # sbpss_gini_score('map_exp_sbpss_30x30_comp', base_cols)
     # comparison_graph5('./Results/grids_exp_parallel_new_9_x_9')
     # sbpss_gini_score('erdos_renyi_sbpss_comp', base_cols)
